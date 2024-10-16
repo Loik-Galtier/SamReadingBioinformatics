a5907ac79dc2798166c0e9a982af73d4bdd0bf6f
Loik Galtier <loik2galtier@gmail.com>
2024-10-16 09:56:40 +0200

    Fusion of the path and name for simplify

--- SamMapScript.py
+++ SamMapScript.py
@@ -2,18 +2,18 @@
 import os
 import re
 
+#aSearching a library wich for browsing folder, mb Tkinter bu lighter if possible
 
-#add Tkinter in the futur for all graphics and add drag and drop
-name = input("Name of the file : ?") #file name (can work with alone)
-path = input("File directory path : ?") #direcory WIHOUT the file name (cant work without name)
+name = input("Path and Name of the file : ?") #Simplify
 
 
-#check the selected path + file exist and have the .sam  #wich tkinter put, the name can be change without changing the path. #or not.
-if os.path.exists(path + name) and re.search(r".sam", name):
+#check the selected file exist and have the .sam
+if os.path.exists(name) and re.search(r".sam", name):
     print("There is a file")
 else:
     print("Don't have a file")
 #need to create def for next time
+
 
 
 #say if you prefer Aicha that i do on english or french
