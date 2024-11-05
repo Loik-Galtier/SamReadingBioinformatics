8bffe1f09a500f87d7a46eae5d210b4f0ae36beb
Loik Galtier <loik2galtier@gmail.com>
2024-11-05 09:12:46 +0100

    Added the def "FindFile" and ask again when not finding the file

--- SamMapScript.py
+++ SamMapScript.py
@@ -1,19 +1,23 @@
+#!/usr/bin/python3
+#-*- coding : utf-8 -*-
 
 import os
 import re
 
 #aSearching a library wich for browsing folder, mb Tkinter bu lighter if possible
 
-name = input("Path and Name of the file : ?") #Simplify
-
-
 #check the selected file exist and have the .sam
-if os.path.exists(name) and re.search(r".sam", name):
-    print("There is a file")
-else:
-    print("Don't have a file")
-#need to create def for next time
+def FindFile ():
+    name = input("Path and Name of the file : ?")
+    if os.path.exists(name) and re.search(r".sam", name):
+        print("There is a file") # continue -> Ask what question to respond (1, 2, 3, 4)
+    else:
+        print("Don't have a file") # Ask again for a file.
+        FindFile()
 
 
 
-#say if you prefer Aicha that i do on english or french
+
+FindFile()
+
+
