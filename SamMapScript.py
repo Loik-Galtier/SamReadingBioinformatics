62cc3531b2bae3d4955cdbbbcfc0bba2e5e7c583
Loik Galtier <loik2galtier@gmail.com>
2024-11-05 10:23:47 +0100

    Open now the sam file

--- SamMapScript.py
+++ SamMapScript.py
@@ -3,21 +3,26 @@
 
 import os
 import re
+import sys
 
-#aSearching a library wich for browsing folder, mb Tkinter bu lighter if possible
 
 #check the selected file exist and have the .sam
 def FindFile ():
-    name = input("Path and Name of the file : ?")
-    if os.path.exists(name) and re.search(r".sam", name):
-        print("There is a file") # continue -> Ask what question to respond (1, 2, 3, 4)
-    else:
-        print("Don't have a file") # Ask again for a file.
-        FindFile()
+    noFileFound = True
+    while noFileFound :
+        name = input("Path and Name of the file : ?")
+        if os.path.exists(name) and re.search(r".sam", name):
+            noFileFound = False
+            print("There is a file") # continue -> Ask what question to respond (1, 2, 3, 4)
+            with open(name, "a+") as SamFile:
+                return SamFile
+
+        else:
+            print("Don't have a file") # Ask again for a file.
 
 
 
 
-FindFile()
 
+SamFile = FindFile()
 
