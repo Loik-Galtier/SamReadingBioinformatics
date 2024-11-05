1fc883412b3caac9d753f995949516ec8be4fd98
Loik Galtier <loik2galtier@gmail.com>
2024-11-05 14:11:01 +0100

    Quality upgrade, and added count of unmapped reads

--- SamMapScript.py
+++ SamMapScript.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 #-*- coding : utf-8 -*-
-
 import os
 import re
 import sys
@@ -18,6 +17,14 @@ def findFile ():
         else:
             print("Don't have a file") # Ask again for a file.
 
+def chooseParameter():
+    print("Choose a option to add for all next operation:")
+    print("0 : no special parameter")
+    print("1 : Only mapped")
+    print("2 : no unmapped")
+    print("3 : Max quality of X")
+    parameter = input("?")
+    return parameter
 
 def flagBits(flag) : #Used same def of the template because is good and if we have to keep the table of 2¹⁶ for analyzing, we gonna do the same part (of convert in bits and set to the 12 line of the table) #look after for change ?
 
@@ -28,10 +35,10 @@ def flagBits(flag) : #Used same def of the template because is good and if we ha
         add = 12 - len(flagB) # We compute the difference between the maximal flag size (12) and the length of the binary flag.
         for t in range(add):
             flagB.insert(0,'0') # We insert 0 to complete until the maximal flag size.
-    print(flagB)
+
     return flagB
 
-def CountDifferentFlag(SamFile):
+def CountReadFlag():
     Occurence = {} #dico
     with open(name, "rt") as SamFile:
         for line in SamFile:
@@ -43,10 +50,22 @@ def CountDifferentFlag(SamFile):
                     Occurence[Field_line[1]] = 1
         print(Occurence)
 
+def WhoUnMapped():
+    Count = 0
+    with open(name, "rt") as SamFile:
+        for line in SamFile:
+            if not re.search(r"^@", line): #if is not a line commentary
+                Field_line = line.split("\t")  # separate for every collone
+                flag = flagBits(Field_line[1])  # take the 2nd (0 1) field and pu it in binnary
 
+                if int(flag[-3]) == 1:  # If the 3rd bits (from the last) is = to 1, so the flag is unmapped
+                    Count += 1  # add one Unmapped count
 
+        return Count
 
 ### Start ###
 name = findFile()
+parameter = chooseParameter()
 print(name)
-CountDifferentFlag(name)
+CountReadFlag()
+print(WhoUnMapped())
